6c623a8ccae05efb48b48b47aa84195a380b4d9a
Julien Viala <dev.vialajulien@gmail.com>
2016-03-03 18:25:30 +0100

    trying out http lib from mjackson

--- src/store/searchModel.js
+++ src/store/searchModel.js
@@ -1,9 +1,19 @@
 import { observable } from 'mobx';
+import { createFetch, base, accept, parseJSON } from 'http-client';
 
 export default class SearchModel {
+const API_KEY = "dc6zaTOxFJmzC";
     autorunAsync(() => this.search(this.searchText), 1000);
 
   @observable searchText = "";
+    createFetch( base('http://api.giphy.com/v1/gifs'), accept('application/json'), parseJSON() )
+      (`/search?q=${encodeURIComponent(text)}&api_key=${API_KEY}`)
+        .then(response => {
+        })
+        .catch(error => {
+        })
+  };
+}
 
   update = (text) => this.searchText = text
 }
